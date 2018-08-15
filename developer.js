a05c49176cd3451f4ddad3ce472c86c45c2f3062
Jeff Prestes <jeffprestes@gmail.com>
2018-08-15 16:21:43 -0300

    Saves new developer

--- developer.js
+++ developer.js
@@ -27,3 +27,37 @@ function getTotalDevelopers() {
         }
     });    
 }
+
+function startOverNewDeveloper() {
+    var frm = document.frmDeveloper;
+    frm.devVotes.value = "";
+    frm.devCommission.value = "";
+    frm.devAddress.value = "";
+    $("#btnStartOverNewDeveloper").hide();
+    $("#btnInsertNewDeveloper").show();
+    $("#statusFormDeveloper").css("background-color", "lightgray");
+    $("#statusFormDeveloper").html("");
+}
+
+function insertNewDeveloper() {
+    var frm = document.frmDeveloper;
+    if (frm.devAddress.value.length < 40 || frm.devCommission.value.length < 2 || frm.devVotes.value.length < 1) {
+        $("#statusFormDeveloper").css("background-color", "lightred");
+        $("#statusFormDeveloper").html("You must inform appropriate information");
+        return
+    }
+    $("#statusFormDeveloper").css("background-color", "lightblue");
+    $("#statusFormDeveloper").html("Waiting for you to confirm the transaction in MetaMask or another Ethereum wallet software");
+    console.log("Sending..." + frm.devAddress.value + " and " + frm.devCommission.value + " and " + frm.devVotes.value);
+    contract.newDeveloper(frm.devAddress.value, frm.devCommission.value, frm.devVotes.value, {from: web3.eth.accounts[0], gas: 3000000, value: 0}, function (err, result) {
+        if (!err) {
+            $("#statusFormDeveloper").css("background-color", "yellow");
+            $("#statusFormDeveloper").text("Transaction sent. Wait until it is mined. Transaction hash: " + result);
+            waitForTxToBeMined(result, "#statusFormDeveloper");
+        } else {
+            console.error(err);
+        }
+    });
+    $("#btnStartOverNewCategory").show();
+    $("#btnInsertNewCategory").hide();
+}
--- start.js
+++ start.js
@@ -1,3 +1,4 @@
 //getDeveloperDetail("0x263C3Ab7E4832eDF623fBdD66ACee71c028Ff591");
 $("#btnStartOverNewCategory").hide();
+$("#btnStartOverNewDeveloper").hide();
 getTotalDevelopers();
