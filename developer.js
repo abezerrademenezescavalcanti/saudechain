ac677ca5bb0b6d4d0db4c7987ed33acaad9a6638
Jeff Prestes <jeffprestes@gmail.com>
2018-12-17 20:35:32 -0200

    adjustments for mainnet

--- developer.js
+++ developer.js
@@ -25,7 +25,8 @@ function getTotalDevelopers() {
             console.error(err);
             $("#dev").html(err);
         }
-    });    
+    });  
+    return;  
 }
 
 function startOverNewDeveloper() {
@@ -48,7 +49,7 @@ function insertNewDeveloper() {
     $("#statusFormDeveloper").css("background-color", "lightblue");
     $("#statusFormDeveloper").html("Waiting for you to confirm the transaction in MetaMask or another Ethereum wallet software");
     console.log("Sending..." + frm.devAddress.value + " and " + frm.devCommission.value);
-    devContract.newDeveloper(frm.devAddress.value, frm.devCommission.value, {from: web3.eth.accounts[0], gas: 3000000, value: 0}, function (err, result) {
+    devContract.newDeveloper(frm.devAddress.value, frm.devCommission.value, true, {from: web3.eth.accounts[0], gas: 3000000, value: 0}, function (err, result) {
         if (!err) {
             $("#statusFormDeveloper").css("background-color", "yellow");
             $("#statusFormDeveloper").text("Transaction sent. Wait until it is mined. Transaction hash: " + result);
